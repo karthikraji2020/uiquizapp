c5b559df5f9374a729efc1f3430b2826add5efb9
karthikraji2020 <karthikraji2020@gmail.com>
2020-07-05 00:01:02 +0530

    ui added layout

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
+  startQuiz() {
+    this.router.navigateByUrl('/quiz');
+    console.log('from startQuiz');
+  }
+/*
+ {
+        "topics":["html","css","javascript","angularjs","angular7","reactjs","typescript"]
+    },
+    */ 
 
 }
--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,23 +1,89 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,AfterViewInit} from '@angular/core';
 import { QuizService } from 'src/app/service/quiz.service';
+import { QuizQuestion } from 'src/app/service/models/questions.interface';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
-export class QuizComponent implements OnInit {
+export class QuizComponent implements OnInit,AfterViewInit {
 
   topics:[]=[];
+  allQuestions: QuizQuestion[] = [];
+  timeLeft: number = 60;
+  interval;
+
+  shuffledQuestions: QuizQuestion[] = [];
+  currentQuestionIndex:number=0;
+  totalQuestions:number;
 
   constructor(private quizService:QuizService) { 
+  this.getAllQuestion();
       this.quizService.getTopics().subscribe((data:[])=>{
         this.topics=data;
         console.log(data);
       });
   }
 
-  ngOnInit() {
+  getAllQuestion() {
+    this.quizService.getQuestions().subscribe((data:QuizQuestion[])=>{
+      this.allQuestions=data;
+      console.log(data);
+    });
   }
+
+  ngOnInit() {
+    this.shuffledQuestions = this.allQuestions.sort(() => Math.random() - .5);
+    this.totalQuestions=this.shuffledQuestions.length;
+    this.currentQuestionIndex = 0;
+  }
+  
+   startQuiz() {
+    this.showQuestion(this.shuffledQuestions[this.currentQuestionIndex]);
+  }
+
+  navigateToNextQuestion() {
+    this.resetState();
+    this.currentQuestionIndex++
+    this.showQuestion(this.shuffledQuestions[this.currentQuestionIndex]);
+    this.startTimer();
+  }
+  resetState () {
+    this.timeLeft = 60;
+  }
+  showQuestion(question) {
+
+  if (typeof document.getElementById('question') !== 'undefined' && this.currentQuestionIndex <= this.totalQuestions) {
+    document.getElementById('question').innerHTML = this.shuffledQuestions[this.currentQuestionIndex]['questionText'];
+    document.getElementById('question').style.border = "3px solid blue";
+  } else {
+
+    // this.navigateToResults();
+  }
+
+  }
+
+
+startTimer() {
+
+    this.interval = setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        this.navigateToNextQuestion();
+        this.timeLeft = 60;
+      }
+    },1000)
+  }
+
+  pauseTimer() {
+    clearInterval(this.interval);
+  }
+
+  ngAfterViewInit() {
+    this.startTimer();
+  }
+  
 
 }
