fbaa5f19b723856c17806c794bfd6f3ea8bffdc7
karthikraji2020 <karthikraji2020@gmail.com>
2020-07-07 19:49:40 +0530

    ui minor

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -13,32 +13,38 @@ export class QuizComponent implements OnInit,AfterViewInit {
   allQuestions: QuizQuestion[] = [];
   timeLeft: number = 60;
   interval;
+  options =[];
 
   shuffledQuestions: QuizQuestion[] = [];
   currentQuestionIndex:number=0;
   totalQuestions:number;
 
   constructor(private quizService:QuizService) { 
-  this.getAllQuestion();
       this.quizService.getTopics().subscribe((data:[])=>{
         this.topics=data;
         console.log(data);
+        if(data) {
+          this.getAllQuestion();
+        }
+
       });
   }
-
+  ngOnInit() {
+   }
+   
   getAllQuestion() {
     this.quizService.getQuestions().subscribe((data:QuizQuestion[])=>{
       this.allQuestions=data;
       console.log(data);
+      if(data) {
+        this.shuffledQuestions = this.allQuestions.sort(() => Math.random() - .5);
+        this.totalQuestions=this.shuffledQuestions.length;
+        this.currentQuestionIndex = 0;
+      }
     });
-  }
-
-  ngOnInit() {
-    this.shuffledQuestions = this.allQuestions.sort(() => Math.random() - .5);
-    this.totalQuestions=this.shuffledQuestions.length;
-    this.currentQuestionIndex = 0;
-  }
   
+  }
+ 
    startQuiz() {
     this.showQuestion(this.shuffledQuestions[this.currentQuestionIndex]);
   }
@@ -52,11 +58,21 @@ export class QuizComponent implements OnInit,AfterViewInit {
   resetState () {
     this.timeLeft = 60;
   }
+  choosedOption(opt) {
+    console.log(opt.optionValue)
+
+    if(opt.optionValue){
+
+    }
+    
+
+  }
   showQuestion(question) {
 
   if (typeof document.getElementById('question') !== 'undefined' && this.currentQuestionIndex <= this.totalQuestions) {
     document.getElementById('question').innerHTML = this.shuffledQuestions[this.currentQuestionIndex]['questionText'];
-    document.getElementById('question').style.border = "3px solid blue";
+    // document.getElementById('question').style.border = "3px solid blue";
+    this.options=this.shuffledQuestions[this.currentQuestionIndex]['options'];
   } else {
 
     // this.navigateToResults();
