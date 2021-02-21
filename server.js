46cbbd35126b4de2096673ad64ce44930c652033
karthikraji2020 <karthikraji2020@gmail.com>
2021-02-21 13:43:43 +0530

    tested

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('./dist/uiquizapp'));
-
+app.use(express.json());
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/uiquizapp/'}),
 );
